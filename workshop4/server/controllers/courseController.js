098539210972b301fff794eddd72a6a5fd8ce799
jmm7gameover <jmm7gameover@gmail.com>
2024-06-17 15:12:32 -0600

    agregando documentacion para el workshop 4

--- workshop4/server/controllers/courseController.js
+++ workshop4/server/controllers/courseController.js
@@ -34,36 +34,28 @@ const coursePost = async (req, res) => {
  * @param {*} res
  */
 const courseGet = (req, res) => {
-  if (req.query && req.query.id) {
-    // Get a specific course by ID
-    Course.findById(req.query.id).populate('teacher')
-      .then((course) => {
-        res.json(course);
-      })
-      .catch(err => {
-        res.status(404);
-        console.log('error while querying the course', err);
-        res.json({ error: "Course doesn't exist" });
-      });
-  } else {
-    // Get all courses or filter by name and sort
-    const { name, sort } = req.query;
 
-    // Create filter and sort options
+    const { name, sort } = req.query; // Se extraen los parámetros de consulta 'name' y 'sort' de la solicitud.
+    
+    //filtro para buscar cursos cuyo nombre coincida parcialmente con el valor de name, ignorando mayúsculas y minúsculas.
+    //Si no se proporciona name, el filtro estará vacío ({}).
     const filter = name ? { name: new RegExp(name, 'i') } : {};
+    
+    //Si sort es igual a desc, ordenamos por name en orden descendente { name: -1 }.
+    //De lo contrario, ordenamos en orden ascendente { name: 1 }.
     const sortOption = sort === 'desc' ? { name: -1 } : { name: 1 };
-
-    // Find courses with filter and sort
-    Course.find(filter).populate('teacher').sort(sortOption)
+    
+    Course.find(filter).sort(sortOption)//buscar cursos que coincidan con el filtro y sortOption para ordenar los resultados según la opción especificada
       .then(courses => {
         res.json(courses);
+        // Si se encuentran los cursos, se envía la información de los cursos en formato json
       })
       .catch(err => {
         res.status(422);
         console.log('error while querying the courses', err);
         res.json({ "error": err });
       });
-  }
+  
 };
 
 module.exports = {
