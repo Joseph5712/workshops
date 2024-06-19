1d2fbdb5483003f9aea7d97866e50efce8cea8fa
jmm7gameover <jmm7gameover@gmail.com>
2024-06-18 18:13:10 -0600

    agregando un select para desc y asc

--- workshop4/client/js/functions.js
+++ workshop4/client/js/functions.js
@@ -1,53 +1,45 @@
 function assignEditEvents() {
-    for (let el of document.getElementsByClassName('edit_button')) {
-        el.addEventListener('click', (e) => {
-        console.log(e.target.id);
-        alert(`element with id ${e.target.id} clicked`);
-        e.preventDefault();
-        });
-    }
-  }
-  
-  async function getCourses() {
-    const response = await fetch("http://localhost:3001/api/courses");
-    const teachers = await response.json();
-    //console.log('cousers:', teachers);
-  
-    if(teachers) {
-      const container = document.getElementById('result');
-      container.innerHTML = '';
-      teachers.forEach(element => {
-        const item = document.createElement('li');
-        item.innerHTML = `${element.name} <a href="" class="edit_button" id="${element._id}">Edit</a>`;
-        item.setAttribute('data-id',element._id);
-        container.appendChild(item)
+  for (let el of document.getElementsByClassName('edit_button')) {
+      el.addEventListener('click', (e) => {
+      console.log(e.target.id);
+      alert(`element with id ${e.target.id} clicked`);
+      e.preventDefault();
       });
-  
-      assignEditEvents();
-    }
   }
-  
-  async function createCourse() {
-    let course =  {
-      name: document.getElementById('name').value,
-      credits: document.getElementById('credits').value
-    }
+}
 
-    const response = await fetch("http://localhost:3001/api/courses",{
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(course)
+async function getCourses() {
+  const sortOrder = document.getElementById('sortOrder').value;
+  const response = await fetch(`http://localhost:3001/api/courses?sort=${sortOrder}`);
+  const teachers = await response.json();
+
+  if(teachers) {
+    const container = document.getElementById('result');
+    container.innerHTML = '';
+    teachers.forEach(element => {
+      const item = document.createElement('li');
+      item.innerHTML = `${element.name} <a href="" class="edit_button" id="${element._id}">Edit</a>`;
+      item.setAttribute('data-id',element._id);
+      container.appendChild(item);
     });
-  
-    //debugger;
-    //if(response && response.status == 201){
-    //    course = await response.json();
-    //  console.log('Course saved', course);
-    //  alert('Course guardado');
-    //} else {
-    //  alert("Shit's on fire! ");
-    //}
+
     assignEditEvents();
   }
+}
+
+async function createCourse() {
+  let course = {
+      name: document.getElementById('name').value,
+      credits: document.getElementById('credits').value
+  };
+
+  const response = await fetch("http://localhost:3001/api/courses", {
+      method: "POST",
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(course)
+  });
+
+  assignEditEvents();
+}
